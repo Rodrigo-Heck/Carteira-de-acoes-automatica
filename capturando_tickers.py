79d95c6dee9c0b83806326a0f5952710d7154c95
Rodrigo Heck <rodrigo-hec@hotmail.com>
2021-07-04 21:33:35 -0300

    alteração
    estratégia clube do valor

--- capturando_tickers.py
+++ capturando_tickers.py
@@ -7,7 +7,6 @@ Created on Thu Feb 18 20:15:07 2021
 import requests
 import pandas as pd
 import re
-from datetime import datetime as dt
 
 
 def corrigindo_valores(df):
@@ -20,7 +19,7 @@ def corrigindo_valores(df):
                 df[a][i] = pd.to_numeric(df[a][i], errors="ignore")
 
 
-def busca(url):
+def buscar(url):
     header = {
           "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) "
           "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -33,81 +32,98 @@ def busca(url):
     return(informacoes)
 
 
-def lista_para_busca():
-    empresas_listadas = busca(
+def busca_filtrada():
+    empresas_listadas = buscar(
         url="https://www.fundamentus.com.br/resultado.php"
         )[0]
     corrigindo_valores(df=empresas_listadas)
     for i in range(len(empresas_listadas["Papel"])):
         if (
-            (empresas_listadas["Liq.2meses"][i] < 6000000) or
-            (empresas_listadas["P/L"][i] <= 0) or
-            (empresas_listadas["Cresc. Rec.5a"][i] <= 0)
+            (empresas_listadas["Liq.2meses"][i] < 1000000) or
+            (empresas_listadas["EV/EBIT"][i] <= 0)
         ):
             empresas_listadas = empresas_listadas.drop(i)
     return(empresas_listadas.reset_index(drop=True))
 
 
-def escolhendo_top20(acoes_pesquisadas):
-    a_p = acoes_pesquisadas
-    a_p.sort_values(by=["Desconto"], ascending=False, inplace=True)
-    a_p = a_p.reset_index(drop=True)
-
+def remover_papeis_repetidos(fundamentus):
     papel_list = []
-    corrigir_papeis_repetidos = []
-    for i in range(len(a_p)):
-        papel = a_p["Papel"][i]
+    papeis_repetidos = []
+
+    for i in range(len(fundamentus)):
+        papel = fundamentus["Papel"][i]
         papel = re.sub('[0-9]', '', papel)
         papel_list.append(papel)
-        if(not papel_list.count(papel) > 1):
-            corrigir_papeis_repetidos.append(a_p.iloc[i])
 
-    a_p = pd.DataFrame(corrigir_papeis_repetidos).reset_index(drop=True)
+        if((papel_list.count(papel) > 1) and (papel not in papeis_repetidos)):
+            papeis_repetidos.append(papel)
 
-    setores = []
-    papeis = []
-    limit_number = 15
-    for num in range(len(a_p["Setor"])):
-        if(limit_number == 0):
-            break
-        if(setores.count(a_p["Setor"][num]) < 4):
-            limit_number -= 1
-            setores.append(a_p["Setor"][num])
-            papeis.append(a_p.iloc[num])
+    fundamentus.index = fundamentus["Papel"]
 
-    carteira = pd.DataFrame(papeis).reset_index(drop=True)
+    maior_liquidez = []
 
-    carteira = pd.DataFrame([carteira["Papel"],
-                            carteira["Data últ cot"],
-                            carteira["Cotação"],
-                            carteira["Desconto"],
-                            carteira["Preço justo"],
-                            carteira["Div. Yield"],
-                            carteira["Setor"]]).transpose()
-    nome_xlsx = str("TOP15_" + str(dt.now().date()) + ".xlsx")
-    carteira.to_excel(nome_xlsx, index=False)
-    return(carteira)
+    for ticker in papeis_repetidos:
+
+        escolher_papel = []
+        liquidez = []
+        for i in range(len(fundamentus)):
+            if(re.sub('[0-9]', '', fundamentus["Papel"][i]) == ticker):
+
+                escolher_papel.append(fundamentus["Papel"][i])
+                liquidez.append(
+                    fundamentus.loc[fundamentus["Papel"][i]]["Liq.2meses"]
+                    )
+
+        escolher_papel.remove(escolher_papel[liquidez.index(max(liquidez))])
+
+        for i in escolher_papel:
+            fundamentus = fundamentus.drop(fundamentus.loc[i]["Papel"])
+    return(fundamentus.reset_index(drop=True))
 
 
-def exibir_na_tela(carteira):
-    print("\n\n\n == Ticker ===== Preço ==== Preço Justo ==== Desconto ==\n")
-    for i in range(len(carteira)):
-        if(carteira["Cotação"][i] < 10):
-            cotacao = "0" + str('{:.2f}'.format(carteira["Cotação"][i]))
-        else:
-            cotacao = str('{:.2f}'.format(carteira["Cotação"][i]))
+def ranquear(fundamentus):
 
-        if(carteira["Preço justo"][i] < 10):
-            p_justo = "0" + str('{:.2f}'.format(carteira["Preço justo"][i]))
-        else:
-            p_justo = str('{:.2f}'.format(carteira["Preço justo"][i]))
+    evebit = pd.DataFrame(
+                    [
+                        fundamentus["Papel"],
+                        fundamentus["EV/EBIT"],
+                        fundamentus["ROIC"],
+                    ]
+                     ).transpose()
 
-        if(len(carteira["Papel"][i]) == 6):
-            print("    ", carteira["Papel"][i], "    ",
-                  cotacao, "       ", p_justo, "        ",
-                  str(carteira["Desconto"][i])+"%")
-        else:
-            print("    ", carteira["Papel"][i], "     ",
-                  cotacao, "       ", p_justo, "        ",
-                  str(carteira["Desconto"][i])+"%")
-    input("")
+    evebit = evebit.sort_values(
+        by="EV/EBIT", ascending=True
+        ).reset_index(drop=True)
+
+    evebit.insert(0, "index", evebit.index.tolist())
+
+    roic = pd.DataFrame([
+                   fundamentus["Papel"],
+                   fundamentus["EV/EBIT"],
+                   fundamentus["ROIC"]
+                   ]
+                    ).transpose()
+
+    roic = roic.sort_values(by="ROIC", ascending=False).reset_index(drop=True)
+    roic.insert(0, "index", roic.index.tolist())
+
+    evebit.index = evebit["Papel"]
+    roic.index = roic["Papel"]
+
+    inserir = []
+    for i in range(len(fundamentus["Papel"])):
+        papel = fundamentus["Papel"][i]
+
+        inserir.append([
+                 fundamentus["Papel"][i],
+                 evebit.loc[papel]["index"] + roic.loc[papel]["index"],
+                 fundamentus["EV/EBIT"][i],
+                 fundamentus["ROIC"][i]
+                        ])
+    colun = ["Papel", "Nota", "EV/EBIT", "ROIC"]
+    df_final = pd.DataFrame(columns=colun, data=inserir)
+    df_final = df_final.sort_values(
+        by="Nota", ascending=True
+        )
+
+    return(df_final.reset_index(drop=True))
--- index.py
+++ index.py
@@ -4,119 +4,14 @@ Created on Wed Feb 17 12:24:59 2021
 
 @author: Rodrigo
 """
-
-import os
+import re
 import pandas as pd
 import capturando_tickers as tk
-from datetime import datetime as dt
-import time
 
-os.system('cls')
-print("\n\n\n ===============\n")
-print(" Iniciando a Busca!")
-print("\n ===============")
+busca = tk.busca_filtrada()
 
-base_dados = pd.DataFrame()
+fundamentus = busca
 
-busca = tk.lista_para_busca()["Papel"]
+fundamentus = tk.remover_papeis_repetidos(fundamentus)
 
-contador = 0
-for numero_busca in range(len(busca)):
-
-    ticker = busca[numero_busca]
-    tentativas_consulta = 0
-
-    while(tentativas_consulta < 3):
-        try:
-            acao = tk.busca(
-                url="https://www.fundamentus.com.br/detalhes.php?papel="
-                + ticker
-                )
-            acao[0] = acao[0].transpose()
-            acao[1] = acao[1].transpose()
-            acao[2] = ((acao[2].transpose()).drop(columns=[0]))
-            acao[3] = ((acao[3].transpose()).drop(columns=[0]))
-            acao[4] = ((acao[4].transpose()).drop(columns=[0]))
-
-            filtrando = pd.concat([
-                            acao[0].iloc[0:2].reset_index(drop=True),
-                            acao[0].iloc[2:4].reset_index(drop=True),
-                            acao[1].iloc[0:2].reset_index(drop=True),
-                            acao[1].iloc[2:4].reset_index(drop=True),
-                            acao[2].iloc[4:6].reset_index(drop=True),
-                            acao[2].iloc[2:4].reset_index(drop=True),
-                            acao[2].iloc[0:2].reset_index(drop=True),
-                            acao[3].iloc[0:2].reset_index(drop=True),
-                            acao[3].iloc[2:4].reset_index(drop=True),
-                              ], axis=1, join="inner")
-
-            filtrando.columns = filtrando.iloc[0]
-            filtrando = filtrando.drop(0)
-
-            novo_cabecario = []
-            for i in range(len(filtrando.columns)):
-                if(type(filtrando.columns[i]) == str):
-                    novo_cabecario.append(
-                        filtrando.columns[i].replace('?', '')
-                        )
-                else:
-                    novo_cabecario.append("excluir")
-
-            filtrando.columns = novo_cabecario
-            filtrando = filtrando.drop(columns="excluir")
-            filtrando = filtrando.reset_index(drop=True)
-
-            # tratando numeros do dataframe
-            tk.corrigindo_valores(df=filtrando)
-
-            LPA = float(filtrando["LPA"])
-            VPA = float(filtrando["VPA"])
-
-            # Formula de Benjamin Graham
-            # Valor Intrínseco = raiz quadrada de (22,5 x LPA x VPA)
-            preco_justo = ((22.5 * LPA * VPA) ** (1/2))
-
-            preco_justo = float('{:.2f}'.format(preco_justo))
-            preco = float(filtrando["Cotação"])
-            desconto = float(
-                '{:.2f}' .format(((preco_justo - preco) / preco_justo) * 100)
-                )
-
-            list1 = list(filtrando.iloc[0].values)
-            list1.append(desconto)
-            list1.append(preco_justo)
-            list1.append(0)
-            list2 = list(filtrando.columns)
-            list2.append("Desconto")
-            list2.append("Preço justo")
-            list2.append("Nota")
-
-            filtrando = pd.DataFrame([list2, list1])
-            filtrando.columns = filtrando.iloc[0]
-            filtrando = filtrando.drop(0)
-
-            base_dados = base_dados.append(filtrando, sort=False)
-            os.system('cls')
-            print("\n\n ====================\n")
-            print(" Buscando:", ticker)
-            print("   == ", round((numero_busca * 100) / len(busca)), "% == ")
-            print("\n ====================")
-
-            tentativas_consulta = 3
-        except:
-            tentativas_consulta += 1
-            print("Falha: ", ticker)
-            print("Tentativas de consulta: ", tentativas_consulta)
-            time.sleep(60)
-
-base_dados = base_dados.reset_index(drop=True)
-
-carteira = tk.escolhendo_top20(acoes_pesquisadas=base_dados)
-os.system('cls')
-print("\n\n\n =============================================\n")
-print(" Busca finalizada!")
-print(" As informações também estão disponiveis em um arquivo xlsx")
-print(" Nome do arquivo: TOP15_" + str(dt.now().date()) + ".xlsx")
-print("\n =============================================\n")
-tk.exibir_na_tela(carteira)
-print("\n =============================================\n")
+fundamentus = tk.ranquear(fundamentus)
